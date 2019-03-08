5536777625d7b41911209f869cbfd19f9f287454
Logan <logankeyes1212@gmail.com>
2019-03-07 21:20:41 -0600

    jaws added

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ var express = require("express");
 var exphbs = require("express-handlebars");
 var routes = require("./controllers/burgers_controller.js")
 
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
 var PORT = process.env.PORT || 8080;
+}
 var app = express();
 
 app.use(express.static("public"));
